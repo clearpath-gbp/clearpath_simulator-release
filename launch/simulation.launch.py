b5282c4ed0777d1af99501bfc9e8235f8b491cdc
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-07-08 16:44:12 -0400

    Rebase from 'release/humble/clearpath_gz'

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -35,9 +35,9 @@ ARGUMENTS = [
                           choices=['true', 'false'],
                           description='use_sim_time'),
     DeclareLaunchArgument('generate',
-                      default_value='true',
-                      choices=['true', 'false'],
-                      description='Generate parameters and launch files'),
+                          default_value='true',
+                          choices=['true', 'false'],
+                          description='Generate parameters and launch files'),
 ]
 
 for pose_element in ['x', 'y', 'yaw']:
@@ -62,7 +62,8 @@ def generate_launch_description():
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gz_sim_launch]),
         launch_arguments=[
-            ('world', LaunchConfiguration('world'))
+            ('world', LaunchConfiguration('world')),
+            ('setup_path', LaunchConfiguration('setup_path')),
         ]
     )
 
